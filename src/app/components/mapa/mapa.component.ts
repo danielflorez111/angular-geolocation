da916b93e73271dbde92288bbe5ddaf298b597ea
danielflorez111 <danielflorez111@hotmail.com>
2018-04-30 11:07:00 -0500

    NearbyDevices and FarDevices create

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -4,7 +4,9 @@ import { Marcador } from '../../classes/marcador.class';
 import { DeviceService } from '../../services/device.service';
 import { IDevice } from '../../interfaces/device.interface';
 import { ILocation } from '../../interfaces/location.interface';
+import { MapsAPILoader } from '@agm/core';
 declare var google: any;
+
 
 @Component({
   selector: 'app-mapa',
@@ -14,18 +16,28 @@ declare var google: any;
 export class MapaComponent implements OnInit {
 
   devices: IDevice[] = [];
+  nearbyDevices: IDevice[] = [];
+  farDevices: IDevice[] = [];
   location: ILocation;
   marcadores: Marcador[] = []
   lat: number = 6.2250704;
   lng: number = -75.57404319999999;
 
-  constructor(public snackBar: MatSnackBar, private _deviceService: DeviceService) {
+  constructor(public snackBar: MatSnackBar, private _deviceService: DeviceService, private mapsAPILoader: MapsAPILoader) {
     this.devices = this._deviceService.getDevices();
-    this.setLocation();
-    //this.calculateDistances();
+    //this.setLocation();
+
+    this.location = {
+      lat: this.lat,
+      lng: this.lng
+    };
   }
 
   ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      this.calculateDistances();
+      this.updateDevices();
+    });
   }
 
   setLocation(location?: ILocation) {
@@ -47,7 +59,7 @@ export class MapaComponent implements OnInit {
   dragEnd(event) {
     this.setLocation(event.coords);
     this.calculateDistances();
-    console.log(this.devices);
+    this.updateDevices();
   }
 
   calculateDistances() {
@@ -56,6 +68,11 @@ export class MapaComponent implements OnInit {
       let currentLocation = new google.maps.LatLng(this.location.lat, this.location.lng);
       device.distance = google.maps.geometry.spherical.computeDistanceBetween(deviceLocation, currentLocation);
     });
+  }
+
+  updateDevices() {
+    this.nearbyDevices = this.devices.filter(device => device.distance <= 100);
+    this.farDevices = this.devices.filter(device => device.distance > 100)
   }
 
 }
